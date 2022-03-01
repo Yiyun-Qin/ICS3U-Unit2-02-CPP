b0935e500b4eeb67bede819e2dc659c912fa4c9b
Yiyun Qin <yiyun.qin@mths.ca>
2022-03-01 19:26:24 +0000

    math program

---
+++ math.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2022 Yiyun Qin All rights reserved
+//
+// Created by Yiyun Qin
+// Created at February 2022
+// This is a math program with proper style
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    // This function calculates the area and perimetre of a rectangle
+    int length;
+    int width;
+    int area;
+    int perimetre;
+
+    // input
+    std::cout << "Enter the length of the rectangle: ";
+    std::cin >> length;
+    std::cout << "Enter the width of the rectangle: ";
+    std::cin >> width;
+
+    // process
+    area = length * width;
+    perimetre = 2 * (length + width);
+
+    // output
+    std::cout << std::endl;
+    std::cout << "Area is " << area << " mm²." << std::endl;
+    std::cout << "Perimetre is " << perimetre << " mm." << std::endl;
+
+    std::cout << "\nDone." << std::endl;
+}
